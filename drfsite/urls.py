d2da0988d6937b7f6b3f2c7d17062b099c4f5a1c
igorGavr <ragamuffinrogi@gmail.com>
2023-01-13 12:53:50 +0200

    Авторизация и аутентификация. Session-based authentication

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -23,6 +23,7 @@ from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
